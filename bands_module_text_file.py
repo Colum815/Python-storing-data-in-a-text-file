c264384e4720818ef8ffde04ae419f819e1c86b9
Colum <56079750+Cullie815@users.noreply.github.com>
2021-09-16 14:55:15 +0100

    Update bands_module_text_file.py

--- bands_module_text_file.py
+++ bands_module_text_file.py
@@ -1,11 +1,14 @@
 bands_info = 'band_info.txt'
+
+
 def create_file_if_empty():
-    with open(bands_info,"w")as file:
+    with open(bands_info, "w")as file:
         pass
-def band_info(band,album,producer):
+
+
+def band_info(band, album, producer):
     with open(bands_info, "a")as file:
         file.write(f"{band},{album},{producer},no\n")
-
 
 
 def list_band_info():
@@ -13,7 +16,7 @@ def list_band_info():
         lines = [line.strip().split(',') for line in file.readlines()]
 
         bands = [
-            {'name':line[0],'album':line[1],'producer':line[2],'listened to: ':line[3]}
+            {'name': line[0], 'album': line[1], 'producer': line[2], 'listened to: ': line[3]}
 
             for line in lines
 
@@ -21,10 +24,12 @@ def list_band_info():
 
         return bands
 
+
 def _save_all_info(bands):
-    with open(bands_info,"w")as file:
+    with open(bands_info, "w")as file:
         for band in bands:
             file.write(f"{band['name']},{band['album']},{band['producer']},{band['listened to: ']}\n")
+
 
 def listened_to_album_info(user):
     bands = list_band_info()
@@ -34,10 +39,10 @@ def listened_to_album_info(user):
     _save_all_info(bands)
 
 
-
 def delete_band_info(user):
     bands = list_band_info()
     bands = [band for band in bands if band['name'] != user]
     _save_all_info(bands)
+
 
 
